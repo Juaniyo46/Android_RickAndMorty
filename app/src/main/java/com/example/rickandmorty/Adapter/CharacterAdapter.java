de64b9e81a12fe89bb3d9299e18104fe7aa5f0f1
Juaniyo46 <juan.alvarez@escuelaestech.es>
2019-05-23 14:28:25 +0200

    Terminando de meter atributos del json y funcionando

--- app/src/main/java/com/example/rickandmorty/Adapter/CharacterAdapter.java
+++ app/src/main/java/com/example/rickandmorty/Adapter/CharacterAdapter.java
@@ -1,13 +1,16 @@
 package com.example.rickandmorty.Adapter;
 
 import android.content.Context;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.rickandmorty.API.Result;
 import com.example.rickandmorty.R;
 
@@ -47,6 +50,11 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
         TextView tvId;
         TextView tvName;
         TextView tvStatus;
+        TextView tvSpecies;
+        TextView tvType;
+        TextView tvGender;
+        ImageView ivImage;
+
 
         public CharacterViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -54,6 +62,11 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
             tvId = itemView.findViewById(R.id.id);
             tvName = itemView.findViewById(R.id.name);
             tvStatus = itemView.findViewById(R.id.status);
+            tvSpecies = itemView.findViewById(R.id.species);
+            tvType = itemView.findViewById(R.id.type);
+            tvGender = itemView.findViewById(R.id.gender);
+            ivImage = itemView.findViewById(R.id.ivImage);
+
         }
 
         public void bindUsuario(Result character) {
@@ -61,6 +74,17 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
             tvName.setText(name);
             String status = character.getStatus();
             tvStatus.setText(status);
+            String species = character.getSpecies();
+            tvSpecies.setText(species);
+            String type = character.getType();
+            tvType.setText(type);
+            String gender = character.getGender();
+            tvGender.setText(gender);
+
+            Glide.with(context).load(character.getImage())
+                    .into(ivImage);
+
+             //image = character.getImage();
             tvId.setText(String.valueOf(character.getId()));
         }
     }
